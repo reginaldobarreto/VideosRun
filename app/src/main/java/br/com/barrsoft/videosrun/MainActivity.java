0a0c8e9a164e7859d7e9e0e4ebec791d6f558459
Reginaldo Barreto <reginaldo.barreto@gmail.com>
2019-12-01 12:04:08 -0300

    videoview configurada

--- app/src/main/java/br/com/barrsoft/videosrun/MainActivity.java
+++ app/src/main/java/br/com/barrsoft/videosrun/MainActivity.java
@@ -2,13 +2,32 @@ package br.com.barrsoft.videosrun;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.imageViewPlay)
+    ImageView imageViewPlay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+    }
+
+
+    @OnClick(R.id.imageViewPlay)
+    public void imageViewPlay(View view){
+        Intent intent = new Intent(this,MovieActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/br/com/barrsoft/videosrun/MovieActivity.java
+++ app/src/main/java/br/com/barrsoft/videosrun/MovieActivity.java
@@ -3,12 +3,27 @@ package br.com.barrsoft.videosrun;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MovieActivity extends AppCompatActivity {
+
+    @BindView(R.id.videoView)
+    VideoView videoView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie);
+
+        ButterKnife.bind(this);
+
+        videoView.setMediaController(new MediaController(this));
+        videoView.setVideoPath("android.resource://" + getOpPackageName() + "/" + R.raw.video);
+        videoView.start();
+
     }
 }
